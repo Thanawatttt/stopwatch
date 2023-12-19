71a94ab7c0165ee8e8e32bb6be5c4fa5b35f0e11
Thanawatttt <89511939+Thanawatttt@users.noreply.github.com>
2023-12-19 15:01:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -97,8 +97,4 @@ function clearTimer() {
   document.getElementById('go').style.display="inline";
   document.getElementById('clear').style.display="none";
   document.addEventListener('keydown', function(event) {
-  // Check if Ctrl + F1 is pressed
-  if (event.ctrlKey && event.key === 'F1') {
-    startTimer();
-  }
-});
+}
